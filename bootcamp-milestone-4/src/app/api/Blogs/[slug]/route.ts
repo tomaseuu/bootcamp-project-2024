a93fef2025a21e0aa492affcd0e807fef216f413
Thomas Le <lethomas998@gmail.com>
2024-12-22 11:42:21 -0800

    Fixed Milestone 4 again

--- bootcamp-milestone-4/src/app/api/Blogs/[slug]/route.ts
+++ bootcamp-milestone-4/src/app/api/Blogs/[slug]/route.ts
@@ -1,24 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/database/db";
+import blogSchema from "@/database/blogSchema";
 
-import { NextRequest, NextResponse } from 'next/server'
-import connectDB from "@/database/db"
-import blogSchema from "@/database/blogSchema"
+export async function GET(
+  req: NextRequest,
+  context: { params: { slug: string } }
+) {
+  await connectDB(); // function from db.ts before
+  const { slug } = context.params; // destructure `params` from `context`
 
-type IParams = {
-		params: {
-			slug: string
-		}
-}
-
-// If { params } looks confusing, check the note below this code block
-export async function GET(req: NextRequest, { params }: IParams) {
-    await connectDB() // function from db.ts before
-		const { slug } = params // another destructure
-
-	   try {
-	        const blog = await blogSchema.findOne({ slug }).orFail()
-	        return NextResponse.json(blog)
-	    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-	    } catch (err) {
-	        return NextResponse.json('Blog not found.', { status: 404 })
-	    }
+  try {
+    const blog = await blogSchema.findOne({ slug }).orFail();
+    return NextResponse.json(blog);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json("Blog not found.", { status: 404 });
+  }
 }
