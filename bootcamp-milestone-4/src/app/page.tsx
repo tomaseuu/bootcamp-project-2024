865d461025d45fc294640881f723f11d45092f1f
Thomas Le <lethomas998@gmail.com>
2025-04-21 08:55:34 -0700

    fixed image

--- bootcamp-milestone-4/src/app/page.tsx
+++ bootcamp-milestone-4/src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <div className={styles.pictureBio}>
           <div className={styles.aboutImage}>
             <Image
-              src="/thomas-headshot.jpeg"
+              src="/thomas-headshot.JPEG"
               alt="Thomas Le Image"
               width={300}
               height={300}
